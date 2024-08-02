44cc5e5edf373b6debe3f5784899cc1702b17245
layergrowth <snapchatterjee@gmail.com>
2024-08-02 07:54:05 +0530

    Lg Lg Lg
    ~Clark dev push

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    typeWriter(document.getElementById('main-text').textContent, 0, 'main-text');
+});
+
+function typeWriter(text, i, elementId) {
+    if (i < text.length) {
+        document.getElementById(elementId).textContent = text.substring(0, i + 1);
+        setTimeout(() => typeWriter(text, i + 1, elementId), 50);
+    }
+}
+
+document.getElementById('searchButton').addEventListener('click', function() {
+    window.open("https://www.google.com/search?q=What+is+anon+in+web3%3F", "_blank");
+});
